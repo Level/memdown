2fed68c8d7465c15ccb221b8d93ddd21965f3885
Rod Vagg <rod@vagg.org>
2013-08-02 21:16:11 +1000

    remove approximate-size tests

--- test.js
+++ test.js
@@ -26,9 +26,6 @@ require('abstract-leveldown/abstract/put-get-del-test').all(factory, test, testC
 require('abstract-leveldown/abstract/batch-test').all(factory, test, testCommon)
 require('abstract-leveldown/abstract/chained-batch-test').all(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/approximate-size-test').setUp(factory, test, testCommon)
-require('abstract-leveldown/abstract/approximate-size-test').args(factory, test, testCommon)
-
 require('abstract-leveldown/abstract/close-test').close(factory, test, testCommon)
 
 require('abstract-leveldown/abstract/iterator-test').all(factory, test, testCommon)
