82db5f2a97cbf63c3dd713d22518799d70ecebe3
Calvin Metcalf <cmetcalf@appgeo.com>
2014-09-29 14:54:33 -0400

    redblack

--- memdown.js
+++ memdown.js
@@ -4,66 +4,110 @@ var inherits          = require('inherits')
   , ltgt              = require('ltgt')
   , noop              = function () {}
   , setImmediate      = global.setImmediate || process.nextTick
+  , createRBT = require('functional-red-black-tree')
 
-function toKey (key) {
-  return typeof key == 'string' ? '$' + key : JSON.stringify(key)
+function gt(value) {
+  return value > this._end
 }
-
-function sortedIndexOf (arr, item) {
-  var low = 0, high = arr.length, mid
-  while (low < high) {
-    mid = (low + high) >>> 1
-    arr[mid] < item ? low = mid + 1 : high = mid
-  }
-  return low
+function gte(value) {
+  return value >= this._end;
+}
+function lt(value) {
+  return value < this._end;
+}
+function lte(value) {
+  return value <= this._end;
 }
 
 function MemIterator (db, options) {
-  AbstractIterator.call(this, db)
-  this._limit   = options.limit
-  this._reverse   = options.reverse
-  this._keys    = []
-  this._options = options
-
-  this._pos = 0
-
-  this._keys = this.db._keys.filter(ltgt.filter(options))
-
-  if (this._reverse)
-    this._keys.reverse()
-
-  if (options.limit > 0)
-    this._keys = this._keys.slice(0, options.limit)
-
+  AbstractIterator.call(this, db);
+  this._limit   = options.limit;
+  if (this._limit === -1) {
+    this._limit = Infinity;
+  }
+  this.keyAsBuffer = options.keyAsBuffer !== false;
+  this.valueAsBuffer = options.valueAsBuffer !== false;
+  this._reverse   = options.reverse;
+  this._options = options;
+  this._done = 0;
+  if (!this._reverse) {
+    this._incr = 'next';
+    this._start = ltgt.lowerBound(options);
+    this._end = ltgt.upperBound(options);
+    if (typeof this._start === 'undefined') {
+      this._tree = db.tree.begin;
+    } if (ltgt.lowerBoundInclusive(options)) {
+      this._tree = db.tree.ge(this._start);
+    } else {
+      this._tree = db.tree.gt(this._start);
+    }
+    if (this._end) {
+      if (ltgt.upperBoundInclusive(options)) {
+        this._test = lte;
+      } else {
+        this._test = lt;
+      }
+    }
+  } else {
+    this._incr = 'prev';
+    this._start = ltgt.upperBound(options);
+    this._end = ltgt.lowerBound(options);
+    if (typeof this._start === 'undefined') {
+      this._tree = db.tree.end;
+    } if (ltgt.upperBoundInclusive(options)) {
+      this._tree = db.tree.le(this._start);
+    } else {
+      this._tree = db.tree.lt(this._start);
+    }
+    if (this._end) {
+      if (ltgt.lowerBoundInclusive(options)) {
+        this._test = gte;
+      } else {
+        this._test = gt;
+      }
+    }
+  }
 }
 
 inherits(MemIterator, AbstractIterator)
 
 MemIterator.prototype._next = function (callback) {
-  var self  = this
-    , key
-    , value
 
-  if (self._pos >= self._keys.length)
-    return setImmediate(callback)
+  if (this._done++ >= this._limit) {
+    return setImmediate(callback);
+  }
+  if (!this._tree.valid) {
+    return setImmediate(callback);
+  }
 
-  key = self._keys[self._pos]
+  var key = this._tree.key;
+  var value = this._tree.value;
+  if (!this._test(key)) {
+    return setImmediate(callback);
+  }
+  if (this.keyAsBuffer) {
+    key = new Buffer(key);
+  }
 
-  value = self.db._store[toKey(key)]
+  if (this.valueAsBuffer){
+    value = new Buffer(value);
+  }
+  this._tree[this._incr]();
+  setImmediate(function () {
+    callback(null, key, value);
+  });
+};
 
-  this._pos++
-
-  setImmediate(function () { callback(null, key, value) })
-}
+MemIterator.prototype._test = function () {
+  return true;
+};
 
 function MemDOWN (location) {
   if (!(this instanceof MemDOWN))
     return new MemDOWN(location)
 
   AbstractLevelDOWN.call(this, typeof location == 'string' ? location : '')
-  this._store = {}
-  this._keys  = []
-  this._len = 0;
+  this.tree = createRBT();
 }
 
 inherits(MemDOWN, AbstractLevelDOWN)
@@ -74,21 +118,16 @@ MemDOWN.prototype._open = function (options, callback) {
 }
 
 MemDOWN.prototype._put = function (key, value, options, callback) {
-  var ix = sortedIndexOf(this._keys, key)
-  if (ix >= this._len || this._keys[ix] != key) {
-    this._keys.splice(ix, 0, key)
-    this._len++;
-  }
-  key = toKey(key) // safety, to avoid key='__proto__'-type skullduggery 
-  this._store[key] = value
+  this.tree = this.tree.remove(key).insert(key, value)
   setImmediate(callback)
 }
 
 MemDOWN.prototype._get = function (key, options, callback) {
-  var value = this._store[toKey(key)]
+  var value = this.tree.get(key)
   if (value === undefined) {
     // 'NotFound' error, consistent with LevelDOWN API
-    return setImmediate(function () { callback(new Error('NotFound')) })
+    var err = new Error('NotFound')
+    return setImmediate(function () { callback(err) })
   }
   if (options.asBuffer !== false && !Buffer.isBuffer(value))
     value = new Buffer(String(value))
@@ -98,12 +137,7 @@ MemDOWN.prototype._get = function (key, options, callback) {
 }
 
 MemDOWN.prototype._del = function (key, options, callback) {
-  var ix = sortedIndexOf(this._keys, key)
-  if (this._keys[ix] == key) {
-    this._keys.splice(ix, 1)
-    this._len--;
-  }
-  delete this._store[toKey(key)]
+  this.tree = this.tree.remove(key)
   setImmediate(callback)
 }
 
@@ -113,18 +147,20 @@ MemDOWN.prototype._batch = function (array, options, callback) {
     , key
     , value
     , len = array.length
-
+  function errorCall() {
+    callback(err);
+  }
   while (++i < len) {
     if (array[i]) {
       key = Buffer.isBuffer(array[i].key) ? array[i].key : String(array[i].key)
-      err = this._checkKeyValue(key, 'key')
-      if (err) return setImmediate(function () { callback(err) })
+      err = this._checkKey(key, 'key')
+      if (err) return setImmediate(errorCall)
       if (array[i].type === 'del') {
         this._del(array[i].key, options, noop)
       } else if (array[i].type === 'put') {
         value = Buffer.isBuffer(array[i].value) ? array[i].value : String(array[i].value)
-        err = this._checkKeyValue(value, 'value')
-        if (err) return setImmediate(function () { callback(err) })
+        err = this._checkKey(value, 'value')
+        if (err) return setImmediate(errorCall)
         this._put(key, value, options, noop)
       }
     }
